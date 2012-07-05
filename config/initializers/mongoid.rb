b93eb4acb8c0236590029360b847f4934af8d97e
hiro88hyo <hiro88hyo@gmail.com>
2012-07-05 20:12:38 +0900

    mongoid.rb

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -5,11 +5,11 @@ Mongoid.configure do |config|
 	if ENV['MONGOHQ_URL']
 		uri = URI.parse(ENV['MONGOHQ_URL'])
 		conn = Mongo::Connection.from_uri(ENV['MONGO_HQ'])
-		config.master = conn.db(uri.path.gsub(/^¥//,''))
+		config.master = conn.db(uri.path.gsub(/^\//, ''))
 	else
 		env = Siatra::Application.environment rescue nil
-		name == env == :test ? 'test':'development'
+		#name = env == :test ? 'test' : 'development'
 		host = 'localhost'
-		config.master = Mongo::Connection.new.db(name)
+		config.master = Mongo::Connection.new.db('learning')
 	end
 end
