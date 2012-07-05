499262498eefe7d41447b9bf5a899e2301776ecb
hiro88hyo <hiro88hyo@gmail.com>
2012-07-05 20:25:57 +0900

    fix

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -3,7 +3,7 @@ require 'mongoid'
 
 Mongoid.configure do |config|
 	if ENV['MONGOHQ_URL']
-		puts "#{ENV['MONGOHQ_URL}"
+		puts "#{ENV['MONGOHQ_URL']}"
 		uri = URI.parse(ENV['MONGOHQ_URL'])
 		conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 		db = conn.db(uri.path.gsub(/^\//, ''))
