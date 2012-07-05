311ec800c37d1be7421c5194da171566f704264a
hiro88hyo <hiro88hyo@gmail.com>
2012-07-05 20:10:05 +0900

    horse info

--- app.rb
+++ app.rb
@@ -1,7 +1,28 @@
+# encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require './models'
 
-get '/' do
-  erb :index
+configure do
+	load File.expand_path('../config/initializers/mongoid.rb',__FILE__)
 end
 
+helpers do
+	def horses
+		items = Array.new
+		Owner.find_horses_by_owner("エトゥ").asc(:year).each{|h|
+			items.push(h.horse['name'])
+		}
+		return items
+	end
+end
+
+get '/' do
+	@items = horses()
+	erb :index
+end
+
+get '/horse/:nkid' do
+	@horse = Owner.find_by_nkid(params[:nkid].to_i)
+	erb :horse	
+end
