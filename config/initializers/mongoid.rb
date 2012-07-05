540b1bfd8cf6d463bca1e18137f1b621da19f3cf
hiro88hyo <hiro88hyo@gmail.com>
2012-07-05 20:39:56 +0900

    test

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 require 'mongoid'
 
+puts ENV['MONGOHQ_URL']
+
 Mongoid.configure do |config|
 	if ENV['MONGOHQ_URL']
-		puts "#{ENV['MONGOHQ_URL']}"
 		uri = URI.parse(ENV['MONGOHQ_URL'])
 		conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 		db = conn.db(uri.path.gsub(/^\//, ''))
